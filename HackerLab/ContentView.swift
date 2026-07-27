155bfcc0301be2e88936af24496b39a1279b54dd
Gisela-Camacho-H <gis25cam@gmail.com>
2026-07-27 16:55:18 -0600

    first commit

---
+++ HackerLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackerLab
+//
+//  Created by Gis Cam on 27/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackerLab/HackerLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerLabApp.swift
+//  HackerLab
+//
+//  Created by Gis Cam on 27/07/26.
+//
+
+import SwiftUI
+
+@main
+struct HackerLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
